3414c99ce9ee52ccda63f94aa5b13638f285f9b1
Geoff Shannon <geoffpshannon@gmail.com>
2018-10-04 01:13:18 -0700

    Stub out main request accepting loop

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,47 @@
 #![feature(await_macro, async_await, futures_api)]
 #![allow(dead_code, unused_variables)]
 
-// #[macro_use]
-// extern crate tokio;
-
 use std::net::SocketAddr;
 
+use log::{error, info};
+
+use tokio::await;
 use tokio::net::{UdpFramed, UdpSocket};
-// use tokio::prelude::*;
+use tokio::prelude::*;
 
-use tftp::TftpServer;
+use tftp::{TftpServer, Request};
 
-fn main() {
-    let addr: SocketAddr = "0.0.0.0:69".parse().unwrap();
-    let listener = UdpSocket::bind(&addr).unwrap();
-    let _stream = UdpFramed::new(listener, TftpServer::new());
-
-    tokio::run_async(async {
-    });
-}
-
-#[derive(Clone)]
+#[derive(Clone, Debug, PartialEq)]
 struct Tid(u16);
 
 impl Tid {
     pub fn new(val: u16) -> Tid {
         Tid(val)
     }
+}
+
+struct Connection {
+    tid: Tid,
+    client_tid: Tid,
+}
+
+fn main() {
+    tokio::run_async(async {
+        let addr: SocketAddr = "0.0.0.0:69".parse().unwrap();
+        let listener = UdpSocket::bind(&addr).unwrap();
+        let mut stream = UdpFramed::new(listener, TftpServer::new());
+
+        while let Some(Ok((packet, addr))) = await!(stream.next()) {
+            match packet {
+                Err(e) => error!("{:?}", e),
+                Ok(request) => {
+                    info!("{:?}", request);
+                    match request {
+                        Request::Read(parts) => (),
+                        Request::Write(parts) => (),
+                    }
+                }
+            }
+        }
+    });
 }
