9e236499fe9c04b0ee35c4f5b8c6763d8cda6264
Geoff Shannon <geoffpshannon@gmail.com>
2018-10-07 18:15:14 -0700

    Stub out core read and write loops

--- src/main.rs
+++ src/main.rs
@@ -51,29 +51,40 @@ impl Tid {
 
 struct Connection {
     tid: Tid,
-    client_tid: Tid,
+    client_addr: SocketAddr,
 }
 
 impl Connection {
-    pub fn new(addr: &SocketAddr) -> Connection {
+    pub fn new(client_addr: SocketAddr) -> Connection {
         let tid = Tid::random();
-        let client_tid = Tid::new(addr.port());
-        Connection { tid, client_tid }
+        Connection { tid, client_addr }
     }
 }
 
 fn handle_read(conn: Connection, file: ReadFile) -> () {
+    tokio::run_async(async move {
+        let listener = UdpSocket::bind(&conn.client_addr).unwrap();
+        let mut stream = UdpFramed::new(listener, TftpClient::new());
+        while let Some(Ok((request, addr))) = await!(stream.next()) {
 
+        }
+    })
 }
 
 fn handle_write(conn: Connection, file: WriteFile) -> () {
+    tokio::run_async(async move {
+        let listener = UdpSocket::bind(&conn.client_addr).unwrap();
+        let mut stream = UdpFramed::new(listener, TftpClient::new());
+        while let Some(Ok((request, addr))) = await!(stream.next()) {
 
+        }
+    })
 }
 
 fn handle_request(registry: &mut FileRegistry, addr: SocketAddr, request: Result<Request, DecodeError>) -> Result<(), Error> {
     let request = request?;
     info!("{:?}", request);
-    let connection = Connection::new(&addr);
+    let connection = Connection::new(addr);
 
     match request.r#type() {
         AccessType::Read => {
