a63a5375ab4d7d00ef0cc5faefdb0b8f5423ef76
Geoff Shannon <geoffpshannon@gmail.com>
2018-10-07 18:03:36 -0700

    Move decoders into lib root

--- src/decode.rs
+++ src/decode.rs
@@ -1,15 +1,12 @@
-use std::io;
 use std::result::Result as StdResult;
-
-use bytes::{Buf, BytesMut, IntoBuf};
 
 use failure::Fail;
 
-// use tokio::prelude::*;
-use tokio::prelude::future::Either;
-use tokio_io::codec::{/*Encoder, */Decoder};
+use bytes::{Buf, BytesMut, IntoBuf};
 
-type Result<T> = StdResult<T, Error>;
+use tokio::prelude::future::Either;
+
+pub type Result<T> = StdResult<T, Error>;
 
 #[derive(Debug, Fail)]
 pub enum Error {
@@ -153,63 +150,6 @@ fn parse_error_body(buf: &mut BytesMut) -> Result<Packet> {
     let code = split_u16(buf);
     let message = split_string(buf)?;
     Err(Error::ClientErr { code, message })
-}
-
-pub struct TftpClient {
-    received_end: bool,
-}
-
-impl TftpClient {
-    pub fn new() -> TftpClient {
-        TftpClient { received_end: false }
-    }
-}
-
-impl Decoder for TftpClient {
-    type Item = Result<Data>;
-    type Error = io::Error;
-
-    fn decode(&mut self, buf: &mut BytesMut) -> StdResult<Option<Self::Item>, io::Error> {
-        if self.received_end {
-            return Ok(None);
-        }
-
-        match Packet::from_bytes(buf) {
-            None => Ok(None),
-            Some(res) => {
-                let data = res.and_then(|packet| {
-                    let data_res: Result<Data> = Packet::into_data(packet);
-                    data_res.map(|data: Data| {
-                        if let Data::Data(block) = &data {
-                            self.received_end = true;
-                        }
-                        data
-                    })
-                });
-                Ok(Some(data))
-            }
-        }
-    }
-}
-
-pub struct TftpServer {}
-
-impl TftpServer {
-    pub fn new() -> TftpServer {
-        TftpServer {}
-    }
-}
-
-impl Decoder for TftpServer {
-    type Item = Result<Request>;
-    type Error = io::Error;
-
-    fn decode(&mut self, buf: &mut BytesMut) -> StdResult<Option<Self::Item>, io::Error> {
-        match Packet::from_bytes(buf) {
-            None => Ok(None),
-            Some(res) => Ok(Some(res.and_then(Packet::into_request)))
-        }
-    }
 }
 
 #[cfg(test)]
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,72 @@
 #![allow(dead_code, unused_variables)]
 
 mod decode;
-pub use self::decode::{TftpClient, TftpServer, Packet, Request, AccessType, Data, Block, Error as DecodeError};
+pub use self::decode::{Packet, Request, AccessType, Data, Block, Error as DecodeError};
 
 mod registry;
 pub use self::registry::{FileRegistry, ReadFile, WriteFile, Error as RegistryError};
+
+use std::io;
+use std::result::Result as StdResult;
+
+use bytes::BytesMut;
+
+use tokio_io::codec::Decoder;
+
+
+pub struct TftpClient {
+    received_end: bool,
+}
+
+impl TftpClient {
+    pub fn new() -> TftpClient {
+        TftpClient { received_end: false }
+    }
+}
+
+impl Decoder for TftpClient {
+    type Item = decode::Result<Data>;
+    type Error = io::Error;
+
+    fn decode(&mut self, buf: &mut BytesMut) -> StdResult<Option<Self::Item>, io::Error> {
+        if self.received_end {
+            return Ok(None);
+        }
+
+        match Packet::from_bytes(buf) {
+            None => Ok(None),
+            Some(res) => {
+                let data = res.and_then(|packet| {
+                    let data_res: decode::Result<Data> = Packet::into_data(packet);
+                    data_res.map(|data: Data| {
+                        if let Data::Data(block) = &data {
+                            self.received_end = true;
+                        }
+                        data
+                    })
+                });
+                Ok(Some(data))
+            }
+        }
+    }
+}
+
+pub struct TftpServer {}
+
+impl TftpServer {
+    pub fn new() -> TftpServer {
+        TftpServer {}
+    }
+}
+
+impl Decoder for TftpServer {
+    type Item = decode::Result<Request>;
+    type Error = io::Error;
+
+    fn decode(&mut self, buf: &mut BytesMut) -> StdResult<Option<Self::Item>, io::Error> {
+        match Packet::from_bytes(buf) {
+            None => Ok(None),
+            Some(res) => Ok(Some(res.and_then(Packet::into_request)))
+        }
+    }
+}
